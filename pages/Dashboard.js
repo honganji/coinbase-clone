b07b3b4f8e453dbe1dad1b7fb9c26aea5e38b140
github honganji <yujicavs@icloud.com>
2022-05-05 23:18:02 +0900

    first commit

---
+++ pages/Dashboard.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import Header from '../components/Header.js'
+import Main from '../components/Main.js'
+import Sidebar from '../components/Sidebar.js'
+import { ThirdwebSDK } from "@3rdweb/sdk"
+import { ethers } from 'ethers'
+
+const sdk = new ThirdwebSDK(
+    new ethers.Wallet(
+        process.env.NEXT_PUBLIC_METAMASK_KEY,
+        ethers.getDefaultProvider(
+            'rinkeby'
+        )
+    )
+)
+
+const Dashboard = ({ address }) => {
+    const [sanityTokens, setSanityTokens] = useState([])
+    const [thirdWebTokens, setThirdWebTokens] = useState([])
+
+    useEffect(() => {
+        const getSanityAndThirdWebTokens = async () => {
+            const coins = await fetch(
+                "https://mws9roq1.api.sanity.io/v2021-03-25/data/query/production?query=*%5B_type%20%3D%3D%20'coins'%5D%20%7B%0A%20%20name%2C%0A%20%20usdPrice%2C%0A%20%20contractAddress%2C%0A%20%20symbol%2C%0A%20%20logo%0A%7D"
+            )
+            const sanityTokens = (await coins.json()).result
+            setSanityTokens(sanityTokens)
+
+            setThirdWebTokens(
+                sanityTokens.map(token => sdk.getTokenModule(token.contractAddress))
+            )
+
+
+        }
+
+        getSanityAndThirdWebTokens()
+    }, [])
+
+    console.log(thirdWebTokens)
+    console.log(thirdWebTokens[0].balanceOf(address))
+
+    // console.log('Sanity 👉', sanityTokens)
+    // console.log('Thirdweb 👉', thirdWebTokens)
+    return (
+        <Wrapper>
+            <Sidebar />
+            <MainContainer>
+                <Header
+                    walletAddress={address}
+                    sanityTokens={sanityTokens}
+                    thirdWebTokens={thirdWebTokens}
+                />
+                <Main
+                    walletAddress={address}
+                    sanityTokens={sanityTokens}
+                    thirdWebTokens={thirdWebTokens}
+                />
+            </MainContainer>
+        </Wrapper>
+    )
+}
+
+export default Dashboard
+
+const Wrapper = styled.div`
+    display: flex;
+    height: 100vh;
+    width: 100vw;
+    background-color: #0a0b0d;
+    color: white;
+    overflow: hidden;
+`
+
+const MainContainer = styled.div`
+    flex:1;
+`
